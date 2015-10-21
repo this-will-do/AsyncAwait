c79d419c393a514605e746c8be91ad795f851a7a
this-will-do <jshum9@gmail.com>
2015-10-21 16:29:24 -0500

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace AsyncAwait
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            DownloadSynchronous();
+        }
+
+        private void DownloadSynchronous()
+        {
+            Thread.Sleep(5000);
+            txtContent.Text = "Downloaded Synchronously!";
+        }
+    }
+}
